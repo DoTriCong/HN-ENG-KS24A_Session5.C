c269e4961f5151f12da71a4bbbebedea4f6a581c
DoTriCong <tricong5526@gmail.com>
2024-11-26 00:37:31 +0700

    ex5

---
+++ ex5-ss5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int i;
+	int n; 
+	for( i = 1; i <= 9; i++){
+		printf("Bang cuu chuong %d:\n",i);
+	for( n = 1; n <= 10; n++){
+		printf("%d x %d= %d\n", i, n, i * n); 
+	}
+	} 
+	return 0; 
+}
+
+
+
+
+
